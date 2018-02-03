1e9b49fb7a284beab8fd167870ff4d04773ae281
Lucius <879133505@qq.com>
2018-02-03 20:05:06 +0800

    build user

---
+++ src/main/java/com/lucius/user/entity/User.java
@@ -0,0 +1,60 @@
+package com.lucius.user.entity;
+
+public class User {
+
+    private Long id;
+
+    /**/
+    private String account;
+
+    /**/
+    private String passWord;
+
+    /**/
+    private String CommonName;
+
+    /**/
+    private String englishName;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getCommonName() {
+        return CommonName;
+    }
+
+    public void setCommonName(String commonName) {
+        CommonName = commonName;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+}
